c6758c94844bceaa59b0fcd74400d57b0908819d
Daisuke Nishide <daisuke.nishide@awesome-ars-academia.com>
2026-05-19 09:21:38 +0900

    Add pure UserService from Report Controller.

---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,34 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.entity.User;
+import com.example.repository.UserRepository;
+
+@Service
+@Transactional
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional(readOnly = true)
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email).get();
+    }
+
+    @Transactional(readOnly = true)
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
